0afd13a5b362c84447cd6396eb084bef2a972bfe
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-06-04 20:31:01 -0300

    Setting up basic APIs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app/app.controller';
+import { AppService } from './app/app.service';
+import { UsersModule } from './users/users.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
