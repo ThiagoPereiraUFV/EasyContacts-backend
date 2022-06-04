84c32541513525d9a77113ce8b656966f03e15a1
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-06-04 19:01:06 -0300

    Initial commit

--- src/helpers/deleteFile.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export function deleteFile(filepath: string) {
-	try {
-		fs.unlinkSync(filepath);
-	} catch(error) {
-		throw new Error(error);
-	}
-}
--- src/helpers/paths.ts
+++
@@ -1,9 +0,0 @@
-import path from "path";
-
-export function userUploads(fileName: string) {
-	return path.resolve(__dirname, "..", "..", "public", "users", fileName);
-}
-
-export function contactUploads(fileName: string) {
-	return path.resolve(__dirname, "..", "..", "public", "contacts", fileName);
-}
