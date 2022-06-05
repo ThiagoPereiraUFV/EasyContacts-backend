00412c0e74c256da00951a4d46818fd4f9cba942
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-06-04 21:03:09 -0300

    Crating logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app/app.controller';
 import { AppService } from './app/app.service';
 import { UsersModule } from './users/users.module';
 import { ContactsModule } from './contacts/contacts.module';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
 @Module({
   imports: [UsersModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,32 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(LoggerMiddleware.name);
+
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      const { method, originalUrl } = req;
+      const { statusCode, statusMessage } = res;
+
+      const message = `${method} ${originalUrl} ${statusCode} ${statusMessage}`;
+
+      if (statusCode >= 500) {
+        return this.logger.error(message);
+      }
+
+      if (statusCode >= 400) {
+        return this.logger.warn(message);
+      }
+
+      return this.logger.log(message);
+    });
+
+    const { method, originalUrl, body } = req;
+
+    this.logger.log(`${method} ${originalUrl} ${JSON.stringify(body)}`);
+
+    next();
+  }
+}
