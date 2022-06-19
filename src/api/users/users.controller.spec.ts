0aba7b7df797d7753e8a82bbc973eae7603abc9a
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-06-19 14:15:53 -0300

    Creating User controller unit tests

--- src/api/users/users.controller.spec.ts
+++ src/api/users/users.controller.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { mockUser } from './utils/users.mock';
+
+jest.setTimeout(10000);
 
 describe('UsersController', () => {
   let controller: UsersController;
+  const users = [mockUser(), mockUser()];
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
       providers: [UsersService],
@@ -16,5 +20,48 @@ describe('UsersController', () => {
 
   it('UsersController should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('Should create an array of users', async () => {
+    for (const user of users) {
+      const result = await controller.create(user);
+      expect(result).toBeDefined();
+      expect(result).toMatchObject(user);
+      Object.assign(user, result);
+    }
+  });
+
+  it('Should return an array of users', async () => {
+    const result = await controller.findAll();
+    expect(result).toBeDefined();
+    expect(result).toMatchObject(users);
+    // expect(result).toHaveLength(users.length);
+  });
+
+  it('Should return a user', async () => {
+    const result = await controller.findOne(users[0].id);
+    expect(result).toBeDefined();
+    expect(result).toMatchObject(users[0]);
+  });
+
+  it('Should return a second user', async () => {
+    const result = await controller.findOne(users[1].id);
+    expect(result).toBeDefined();
+    expect(result).toMatchObject(users[1]);
+  });
+
+  it('Should update a user', async () => {
+    const result = await controller.update(users[0].id, {
+      name: users[1].name,
+    });
+    expect(result).toBeDefined();
+    expect(result).toMatchObject({ name: users[1].name });
+  });
+
+  it('Should delete users', async () => {
+    for (const user of users) {
+      const result = await controller.remove(user.id);
+      expect(result).toBeDefined();
+    }
   });
 });
