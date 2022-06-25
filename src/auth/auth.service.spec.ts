7e19f065e7244dc2b7c1f3e7e4937bd41a8d8622
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-06-25 17:03:51 -0300

    Fixing unit tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,11 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersModule } from '../api/users/users.module';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { jwt } from './jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        UsersModule,
+        JwtModule.register({
+          secret: jwt.secret,
+          signOptions: { expiresIn: jwt.expiresIn },
+        }),
+      ],
       providers: [AuthService],
     }).compile();
 
