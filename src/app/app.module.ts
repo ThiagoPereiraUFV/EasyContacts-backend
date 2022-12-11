6278adb01b2c28a3f0dc60a9687f544403b8d7c7
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2022-12-11 09:32:56 -0300

    feat(.): adding Sentry to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,22 @@ import { UsersModule } from '../api/users/users.module';
 import { ContactsModule } from '../api/contacts/contacts.module';
 import { LoggerMiddleware } from '../middlewares/logger.middleware';
 import { AuthModule } from '../auth/auth.module';
+import { SentryModule } from '@ntegral/nestjs-sentry';
+import * as packageJson from '../../package.json';
 
 @Module({
-  imports: [UsersModule, ContactsModule, AuthModule],
+  imports: [
+    UsersModule,
+    ContactsModule,
+    AuthModule,
+    SentryModule.forRoot({
+      dsn: process.env.SENTRY_DSN,
+      debug: process.env.NODE_ENV !== 'production',
+      environment: process.env.NODE_ENV,
+      release: packageJson.version,
+      tracesSampleRate: 1.0,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
