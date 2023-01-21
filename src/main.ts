71733ed29d2418383d231142922ab8a0512e30c8
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2023-01-21 16:46:54 -0300

    feat: adding swagger to document API routes

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 import helmet from 'helmet';
+import * as packageJson from './../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('EasyContacts API')
+    .setDescription('EasyContacts API description')
+    .setVersion(packageJson.version)
+    // .addTag('easycontacts')
+    .build();
+  const options: SwaggerDocumentOptions = {
+    deepScanRoutes: true,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
   app.use(helmet());
   app.enableCors({
     origin: process.env.APP_URL,
